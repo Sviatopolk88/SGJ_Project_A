b44f106dbfe085f7139fd5ff2656267022ada20b
Sviatopolk <72782489+Sviatopolk88@users.noreply.github.com>
2022-04-30 15:39:56 +0300

    stalking or patrolling

--- Shooter_for_SGJ/Assets/Scripts/Detection/Detector.cs
+++ Shooter_for_SGJ/Assets/Scripts/Detection/Detector.cs
@@ -7,67 +7,99 @@ public class Detector : MonoBehaviour, IDetector
     public event ObjectDetectedHandler OnGameObjectDetectedEvent;
     public event ObjectDetectedHandler OnGameObjectDetectionReleasedEvent;
 
-    public GameObject[] detectedObjects => _detectedObjects.ToArray();
+    public Transform CurrentPoint => _patrolPoints[_currentPoint];
 
+    [SerializeField] private Transform[] _patrolPoints;
     [SerializeField] private Transform player;
 
     private NavMeshAgent _navMeshAgent;
-
-    private List<GameObject> _detectedObjects = new List<GameObject>();
+    private int _currentPoint = 0;
     private bool _flag = false;
 
     void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent = GetComponentInParent<NavMeshAgent>();
     }
 
     private void Update()
     {
         if (_flag)
+        {
             _navMeshAgent.SetDestination(player.position);
-    }
-
-    public void Detect(IDetectableObject detectableObject)
-    {
-        if (!_detectedObjects.Contains(detectableObject.gameObject))
+        }
+        else
         {
-            detectableObject.Detected(gameObject);
-            _detectedObjects.Add(detectableObject.gameObject);
-
-            OnGameObjectDetectedEvent?.Invoke(gameObject, detectableObject.gameObject);
+            if (HasReached(_navMeshAgent))
+                _navMeshAgent.SetDestination(GetNext().position);
         }
     }
 
-    public void ReleaseDetection(IDetectableObject detectableObject)
+    public bool HasReached(NavMeshAgent agent)
     {
-        if (_detectedObjects.Contains(detectableObject.gameObject))
+        if (!agent.pathPending)
         {
-            detectableObject.DetectionReleased(gameObject);
-            _detectedObjects.Remove(detectableObject.gameObject);
 
-            OnGameObjectDetectionReleasedEvent?.Invoke(gameObject, detectableObject.gameObject);
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    Debug.Log("hasPath: " + agent.hasPath);
+                    return true;
+                }
+            }
         }
+        else
+        {
+            Debug.Log("pathPending: " + agent.pathPending);
+        }
+        return false;
+    }
+    public Transform GetNext()
+    {
+        var point = _patrolPoints[_currentPoint];
+        _currentPoint = (_currentPoint + 1) % _patrolPoints.Length;
+        return point;
+    }
+
+
+
+    public void Detect(IDetectableObject detectablePlayer)
+    {
+        detectablePlayer.Detected(gameObject);
+        _flag = true;
+
+        OnGameObjectDetectedEvent?.Invoke(gameObject, detectablePlayer.gameObject);
+    }
+
+    public void ReleaseDetection(IDetectableObject detectablePlayer)
+    {
+        detectablePlayer.DetectionReleased(gameObject);
+        Debug.Log(_flag);
+        _flag = false;
+
+        OnGameObjectDetectionReleasedEvent?.Invoke(gameObject, detectablePlayer.gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (IsColliderDetectableObject(other, out var detectedObject))
+        if (IsColliderDetectableObject(other, out var detectedPlayer))
         {
-            Detect(detectedObject);
+            Detect(detectedPlayer);
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (IsColliderDetectableObject(other, out var detectedObject))
+        if (IsColliderDetectableObject(other, out var detectedPlayer))
         {
-            ReleaseDetection(detectedObject);
+            ReleaseDetection(detectedPlayer);
         }
     }
-    private bool IsColliderDetectableObject(Collider coll, out IDetectableObject detectedObject)
+    private bool IsColliderDetectableObject(Collider coll, out IDetectableObject detectedPlayer)
     {
-        detectedObject = coll.GetComponent<IDetectableObject>();
+        detectedPlayer = coll.GetComponent<IDetectableObject>();
 
-        return detectedObject != null;
+        return detectedPlayer != null;
     }
 
 }
