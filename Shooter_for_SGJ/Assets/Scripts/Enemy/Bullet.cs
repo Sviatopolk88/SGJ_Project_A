4991aae22a127f7bdbb3caf57fff926f6b772f6b
Sviatopolk <72782489+Sviatopolk88@users.noreply.github.com>
2022-05-03 12:39:20 +0300

    Сборка уровня

--- Shooter_for_SGJ/Assets/Scripts/Enemy/Bullet.cs
+++ Shooter_for_SGJ/Assets/Scripts/Enemy/Bullet.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int Damage = 10;
+    public float Speed = 10f;
+    public float maxD = 0.1f;
+    public float minD = -0.1f;
+
+    [SerializeField] private Transform _player;
+
+    private float _timer = 2f;
+    private Vector3 Spread;
+
+    private void Start()
     {
-        
+        Spread = new Vector3(1, Random.Range(minD, maxD), Random.Range(minD, maxD));
+    }
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
+            Destroy(this.gameObject);
+        else
+        {
+            transform.Translate(0, 0, Speed * Time.deltaTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        IHittable hitObject = other.gameObject.GetComponent<IHittable>();
+        if (hitObject != null)
+        {
+            hitObject.HitObject(this.Damage);
+            Destroy(this.gameObject);
+        }
+        if (other.gameObject.layer == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 }
